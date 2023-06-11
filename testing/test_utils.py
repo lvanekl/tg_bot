15d2e9f90d8ffc8a4211f6628812389d106ed90d
vanek <23.gorjachev.i@gmail.com>
2023-06-11 03:17:33 +0300

    Незначительные изменения

--- testing/test_utils.py
+++ testing/test_utils.py
@@ -21,36 +21,32 @@ def load_test_data(func):
         my_db.new_chat(telegram_chat_id=t_i)
         my_db.edit_chat_settings(cgat_GPT=0, welcome_meme=DEFAULT_WELCOME_MEME_PATH)
 
-        my_db.add_gym(telegram_chat_id=t_i, name="gym1")
-        my_db.add_gym(telegram_chat_id=t_i, name="gym2")
-
         # достанем айдишники новых залов
-        current_gyms = my_db.get_gyms(telegram_chat_id=1111111)
-        g1 = current_gyms[0][0]
-        g2 = current_gyms[1][0]
+        g1 = my_db.add_gym(telegram_chat_id=t_i, name="gym1")[0]
+        g2 = my_db.add_gym(telegram_chat_id=t_i, name="gym2")[0]
 
-        my_db.add_schedule(telegram_chat_id=t_i, weekday=2, sport="s1", gym=g1, time=def_time1)
-        my_db.add_schedule(telegram_chat_id=t_i, weekday=6, sport="s2", gym=g1, time=def_time2)
-        my_db.add_schedule(telegram_chat_id=t_i, weekday=1, sport="s2", gym=g2, time=def_time2)
-        my_db.add_schedule(telegram_chat_id=t_i, weekday=4, sport="s1", gym=g2, time=def_time1)
+        sch1 = my_db.add_schedule(telegram_chat_id=t_i, weekday=2, sport="s1", gym=g1, time=def_time1)[0]
+        sch2 = my_db.add_schedule(telegram_chat_id=t_i, weekday=6, sport="s2", gym=g1, time=def_time2)[0]
+        sch3 = my_db.add_schedule(telegram_chat_id=t_i, weekday=1, sport="s2", gym=g2, time=def_time2)[0]
+        sch4 = my_db.add_schedule(telegram_chat_id=t_i, weekday=4, sport="s1", gym=g2, time=def_time1)[0]
 
-        my_db.add_schedule_correction(telegram_chat_id=t_i, correction_type="move",
+        sch_c1 = my_db.add_schedule_correction(telegram_chat_id=t_i, correction_type="move",
                                       old_date=today, old_time=def_time1, old_gym=g1,
-                                      new_date=today_p1, new_time=def_time1, new_gym=g2)
+                                      new_date=today_p1, new_time=def_time1, new_gym=g2)[0]
         # такого сдвига быть не может - неверный old_time
         # my_db.add_schedule_correction(telegram_chat_id=t_i, correction_type="move",
         #                               old_date=today, old_time=def_time2, old_gym=g1,
         #                               new_date=today_p1, new_time=def_time1, new_gym=g2)
 
-        my_db.add_schedule_correction(telegram_chat_id=t_i, correction_type="remove",
-                                      old_date=today_p2, old_time=def_time2, old_gym=g1)
-        my_db.add_schedule_correction(telegram_chat_id=t_i, correction_type="add",
-                                      new_date=today_m1, new_time=def_time2, new_gym=g1)
+        sch_c2 = my_db.add_schedule_correction(telegram_chat_id=t_i, correction_type="remove",
+                                      old_date=today_p2, old_time=def_time2, old_gym=g1)[0]
+        sch_c3 = my_db.add_schedule_correction(telegram_chat_id=t_i, correction_type="add",
+                                      new_date=today_m1, new_time=def_time2, new_gym=g1)[0]
         # meme
         # admin
         # answer_alternative
         result = func(*args, **kwargs)
-        # my_db.clear_all_tables()
+        my_db.clear_all_tables()
 
         return result
 
