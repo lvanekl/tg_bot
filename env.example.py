c87ab453cd2a2c5084e2effae6ab7c578ef57a16
vanek <23.gorjachev.i@gmail.com>
2023-06-11 15:00:55 +0300

    Предыдущие комиты работать не будут, потому что я забыл запушить env, в котором есть изменения

--- env.example.py
+++ env.example.py
@@ -3,12 +3,14 @@ import os
 token = ""
 openai_token = ""
 
-DEFAULT_WELCOME_MEME_PATH = None
+DEFAULT_WELCOME_MEME_PATH = ""
 DEFAULT_CHAT_GPT_FLAG = 0
-DEFAULT_CHAT_FUNNY_YES_FLAG = 0
 DEFAULT_CHAT_FUNNY_QUESTION_FLAG = 0
+DEFAULT_CHAT_FUNNY_YES_FLAG = 0
+DEFAULT_CHAT_FUNNY_MAYBE_FLAG = 0
+DEFAULT_CHAT_FUNNY_NO_FLAG = 0
 
+TEST_DB_PATH = 'db/test.sqlite3'
 DB_PATH = 'db/db.sqlite3'
 DB_LOG_PATH = os.path.join('logs', 'db_log.log')
 TESTING_LOG_PATH = os.path.join('logs', 'testing_log.log')
-
